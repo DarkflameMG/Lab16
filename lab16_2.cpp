2f85a991addf1c0a0e85188019efd1358f85387f
DarkflameMG <deknan70@gmail.com>
2021-02-09 16:22:18 +0700

    lab16_2 completed

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char* &p,int N){
+	p = new char [N];    
+	for(int i = 0; i < N;i++) p[i] = 'A'+i;
+	p[N] = 0;
 }
 
 int main(){
 	char *p;
-	int N;	
+	int N;
 	cout << "N = ";
 	cin >> N;		
 	myString(p,N);
